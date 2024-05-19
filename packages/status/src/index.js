5a51d90881f6f30fd85d67ae2d5bdf73440a3dc1
Chris Coleman <chris@rot-26.com>
2024-05-18 21:10:28 -0600

    chore: Update server startup message and add logging for initialization

--- packages/status/src/index.js
+++ packages/status/src/index.js
@@ -2,14 +2,17 @@
 // Return 200 OK for all requests
 import http from 'http';
 
+console.log('init');
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('OK');
+    console.log('hit');
 });
 
 // Listen on port 3000
-server.listen(3000, () => {
-    console.log('Server running on port 3000');
+server.listen(3000, '0.0.0.0', () => {
+    console.log('Server listening on http://0.0.0.0:3000');
 });
 
 // If the server is stopped, the process will exit
